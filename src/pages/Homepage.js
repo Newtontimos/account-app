63c0ae3ccdda313d46cd515392f111731c9058e5
timothy omobolaji <newtontimos01@gmail.com>
2024-01-19 20:53:27 +0100

    my bank account

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Homepage({onClick}) {
+  return (
+    <div>
+        <h1>welcome to homepage</h1>
+        <h2>{}</h2>
+
+      
+    </div>
+  );
+}
+
+export default Homepage;
